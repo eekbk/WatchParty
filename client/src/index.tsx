09c6ceb7b1d70d548e0d3bcfd6f39351a173903b
LoganVoisin <flyflameking@gmail.com>
2022-10-03 12:33:56 -0500

    add: basic init for client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,1 +1,38 @@
 // File for root element
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  // createBrowserRouter,
+  // RouterProvider,
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+const { default: App } = require('./app.tsx');
+const { default: Home } = require('./home.tsx');
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//   },
+//   {
+//     path: "/home",
+//     element: <Home />,
+//   },
+// ]);
+
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    {/* <RouterProvider router={router} /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path='home' element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
--- server/index.ts
+++ server/index.ts
@@ -2,15 +2,33 @@
 const express = require('express');
 const { default: user } = require('./routes/user.ts');
 const { default: party } = require('./routes/watchParty.ts');
+import * as path from 'path';
 
 const PORT = process.env.PORT || 4040;
 const app = express();
 
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve('client', 'dist')));
+app.use(express.json());
+
+//routes
 app.use('/user', user);
 app.use('/party', party);
 
 app.get('/', (req, res) => {
-  res.status(200).send('WOOP');
+  res.status(200).send();
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, '..', 'client', 'dist', 'index.html'),
+    (data, err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+    },
+  );
 });
 
 app.listen(PORT, () => {
