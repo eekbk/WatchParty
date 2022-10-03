63773c1c1f116a3e5788fedccc59973526d497ae
LoganVoisin <flyflameking@gmail.com>
2022-10-03 17:06:24 -0500

    add: package updates and outline for video comp

--- client/src/watchParty.tsx
+++ client/src/watchParty.tsx
@@ -1,7 +1,40 @@
-const WatchParty = () :JSX.Element => {
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Card } from 'react-bootstrap';
+const WatchParty = ({ videos }) => {
+  const [video, setVideo] = useState(() => videos ? videos[0] : {})
+  let config = {
+    method: 'get',
+    url: 'http://localhost:4040/party/',
+    headers: { }
+  };
+  
+  useEffect(() => {
+    axios(config)
+  .then((response) => {
+    console.log(response.data);
+    setVideo(response.data)
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+  }, []);
+  
 
   return (
-    <div>HELLO WORLD</div>
+    <>
+      <Card style={{ width: '50rem', height: '25rem' }}>
+      <Card.Img variant='top' src={video.snippet ? video.snippet.thumbnails.high.url : ''} />
+      <Card.Body>
+        <Card.Title>{video.snippet ? video.snippet.title : 'Please Wait'}</Card.Title>
+        <Card.Text>
+          {
+            video.snippet ? video.snippet.description : 'Please Wait'
+          }
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    </>
   )
 }
 export default WatchParty;
