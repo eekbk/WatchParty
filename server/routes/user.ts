3e48c5528d16fb4f27ebe081f974815de62c9b59
eekbk <eekeric@gmail.com>
2022-10-05 11:41:46 -0500

    server/routes/user.ts

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,10 +1,11 @@
 // File for handling user endpoints
 import express, { Request, Response, Router } from 'express';
-import prisma from '../db/index.ts';
+import prisma from '../db/index';
 
 const user: Router = express.Router();
 
 user.get('/', (req: Request, res: Response) => {
+  //
   res.status(200).send(JSON.stringify('WOOP'));
 });
 
