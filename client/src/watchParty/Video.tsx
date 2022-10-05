dd1045d7425ac9543e63179196fc7e2c4ffb0f8c
LoganVoisin <flyflameking@gmail.com>
2022-10-05 10:15:44 -0500

    add: comments for clarity

--- client/src/watchParty/Video.tsx
+++ client/src/watchParty/Video.tsx
@@ -5,15 +5,24 @@ import io from 'socket.io-client';
 const socket = io();
 
 function Video({ videoUrl }: any) {
+  // state vars
   const [isPlaying, setPause] = useState(false);
 
+  // functions
+
+  // pauses all clients
   const pauseVid = () => {
     socket.emit('pause', false);
   };
+  // plays all clients
   const playVid = () => {
     socket.emit('play', true);
   };
+  const testFunc = (data: any) => {
+    console.log(data);
+  };
 
+  // updates once
   useEffect(() => {
     socket.on('pause', (arg: boolean) => {
       setPause(arg);
@@ -25,13 +34,21 @@ function Video({ videoUrl }: any) {
   console.log(isPlaying);
   return (
     <ReactPlayer
+      config={{
+			  youtube: {
+			    playerVars: {
+			      controls: 1,
+			    },
+			  },
+      }}
       onPlay={playVid}
       onPause={pauseVid}
+      onSeek={testFunc}
       playing={isPlaying}
       url={videoUrl}
       style={{
-				  height: '500px',
-				  width: '500px',
+			  height: '500px',
+			  width: '500px',
       }}
     />
   );
