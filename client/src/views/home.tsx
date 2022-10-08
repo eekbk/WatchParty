4dad12dbfa7c5864545b2a443150d7c613c45072
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-10-08 11:39:32 -0500

    added skeleton to home page

--- client/src/views/home.tsx
+++ client/src/views/home.tsx
@@ -1,5 +1,23 @@
-// Conditionally render based on if user is logged in or not
+import { Container } from 'react-bootstrap';
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 function Home(): JSX.Element {
-  return <div>TEEE HEEE</div>;
+  return (
+		<Container>
+			TEEHEEEHE
+			{/* {user ? user.user_name : 'not logged in'} */}
+			<Row>
+				<Col sm={8}>sm=8</Col>
+				<Col sm={4}>sm=4</Col>
+			</Row>
+			<Row>
+				<Col sm>sm=true</Col>
+				<Col sm>sm=true</Col>
+				<Col sm>sm=true</Col>
+			</Row>
+		</Container>
+  );
 }
 export default Home;
