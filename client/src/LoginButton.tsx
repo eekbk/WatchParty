dba54334d4a97092127052895529e50e3d6b8bdd
eekbk <eekeric@gmail.com>
2022-10-08 10:14:17 -0500

    update: uncomment func

--- client/src/LoginButton.tsx
+++ client/src/LoginButton.tsx
@@ -1,19 +1,20 @@
-// import React from 'react';
-// import axios from 'axios';
+import React from 'react';
+import axios from 'axios';
 
-// function LoginButton() {
-//   const handleClick = () => {
-//     axios.get('/auth/google')
-//       .then(({data}) => {
-//         console.log('data:', data);
-//       });
-//   };
+function LoginButton() {
+  const handleClick = () => {
+    axios.get('/auth/google').then(({ data }) => {
+      console.log('data:', data);
+    });
+  };
 
-//   return (
-//     <div>
-//       <button type="button" onClick={handleClick}>Login</button>
-//     </div>
-//   );
-// }
+  return (
+		<div>
+			<button type="button" onClick={handleClick}>
+				Login
+			</button>
+		</div>
+  );
+}
 
-// export default LoginButton;
+export default LoginButton;
