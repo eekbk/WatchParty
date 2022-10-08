0344c436c32f7ea5aa61a4f94b9cceeb47c2d750
eekbk <eekeric@gmail.com>
2022-10-08 00:27:37 -0500

    add: Search page

---
+++ client/src/views/search/Search.tsx
@@ -0,0 +1,42 @@
+import { useContext } from 'react';
+import { Container, Card } from 'react-bootstrap';
+import { SearchContext } from '../../contexts/searchContext';
+
+function Search() {
+  const { usersMatch, partiesMatch, videosMatch } = useContext(SearchContext);
+
+  return (
+		<Container>
+			Search Results
+			<ul>
+				{usersMatch.map((user) => (
+					<Card key={user.id}>
+						<h3>{user.user_name}</h3>
+						<h4>
+followers:
+{user.follows}
+</h4>
+					</Card>
+				))}
+			</ul>
+			<ul>
+				{partiesMatch.map((party) => (
+					<Card key={party.id}>
+						<h3>{party.name}</h3>
+						<p>{party.description}</p>
+					</Card>
+				))}
+			</ul>
+			<ul>
+				{videosMatch.map((video) => (
+					<Card key={video.id}>
+						<h3>{video.title}</h3>
+						<p>{video.description}</p>
+					</Card>
+				))}
+			</ul>
+		</Container>
+  );
+}
+
+export default Search;
