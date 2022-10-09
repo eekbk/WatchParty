f01911c376dbf7525ec5c251a28e9a6c90098c07
Logan-Ward <ward_logan@outlook.com>
2022-10-09 02:06:22 -0500

    feat: updated user and party endpoints

--- server/db/index.ts
+++ server/db/index.ts
@@ -2,33 +2,3 @@
 import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
-
-// const main = async () => {
-//   // funcs interacting with the database
-//   // add user
-//   // add video
-// };
-
-// main()
-//   .catch((err) => {
-//     console.error(err.message);
-//   })
-//   .finally(async () => {
-//     await prisma.$disconnect();
-//   });
-
-/* Test for creating a playlist in the database
-const playlist : any = {
-  id: 'hello',
-  name: 'hello',
-  description: 'hello',
-  thumbnail: 'hello'
-};
-prisma.playlist.create({ data: playlist })
-.then((r) => {
-  console.log(r);
-})
-.catch(err => {
-  console.error(err);
-})
-*/
