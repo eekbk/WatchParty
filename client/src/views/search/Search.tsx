3d02e890998fa43f9ceb522176991962419b81c6
eekbk <eekeric@gmail.com>
2022-10-09 12:05:00 -0500

    wip: conditional rendering

--- client/src/views/search/Search.tsx
+++ client/src/views/search/Search.tsx
@@ -7,12 +7,65 @@ function Search() {
   const { usersMatch, partiesMatch, videosMatch } = useContext(SearchContext);
   const navigate = useNavigate();
 
+  // const [view, setView] = useState('searchResults');
+
   const handleCardClick = (party, kind) => {
     // reroute to watchParty specific watchParty
     if (kind === 'party') {
       navigate('/watchParty', { state: { party } });
+    } else if (kind === 'video') {
+      // send an axios request to find all the rooms with a playlist that contains that video
     }
   };
+
+  // const renderView = () => {
+  //   if (view === 'searchResults') {
+  //     return (
+  //       <Container>
+  //         {!partiesMatch.length && !usersMatch.length && !videosMatch.length ? (
+  //           <h2>No Matches Found</h2>
+  //         ) : (
+  //           []
+  //         )}
+  //         {partiesMatch.length ? <h2>Parties</h2> : []}
+  //         <ul>
+  //           {partiesMatch.map((party) => (
+  //             <Card key={party.id} onClick={() => handleCardClick(party, 'party')}>
+  //               <h3>{party.name}</h3>
+  //               <p>{party.description}</p>
+  //             </Card>
+  //           ))}
+  //         </ul>
+  //         {usersMatch.length ? <h2>Users</h2> : []}
+  //         <ul>
+  //           {usersMatch.map((user) => (
+  //             <Card key={user.id} onClick={() => handleCardClick(user.id, 'user')}>
+  //               <h3>{user.user_name}</h3>
+  //               <h4>
+  //                 followers:
+  //                 {user.follows}
+  //               </h4>
+  //             </Card>
+  //           ))}
+  //         </ul>
+  //         {videosMatch.length ? <h2>Videos</h2> : []}
+  //         <ul>
+  //           {videosMatch.map((video) => (
+  //             <Card
+  //               key={video.id}
+  //               onClick={() => handleCardClick(video.id, 'video')}
+  //             >
+  //               <h3>{video.title}</h3>
+  //               <p>{`${video.description.slice(0, 200)}...`}</p>
+  //             </Card>
+  //           ))}
+  //         </ul>
+  //       </Container>
+  //     );
+  //   } else if (view === 'videoClick') {
+
+  //   }
+  // };
 
   return (
     <Container>
@@ -50,7 +103,7 @@ function Search() {
             onClick={() => handleCardClick(video.id, 'video')}
           >
             <h3>{video.title}</h3>
-            <p>{video.description}</p>
+            <p>{`${video.description.slice(0, 200)}...`}</p>
           </Card>
         ))}
       </ul>
