1e0c5522745ff9593be612e9af09273888729c74
eekbk <eekeric@gmail.com>
2022-10-09 13:17:28 -0500

    add: endpoint for finding party by videoId

--- server/routes/search.ts
+++ server/routes/search.ts
@@ -6,11 +6,8 @@ export const search: Router = express.Router();
 // endpoint for search queries
 search.get('/:q', async (req: Request, res: Response) => {
   // destructure the query from the req.body
-  // const { q } = req.body;
   const { q } = req.params;
   const qSearch = q.replace(/&/g, ' | ');
-  // console.log('qsearch:', qSearch);
-  // const qSearch = q.replaceAll('&', ' | ');
   // query the database for videos with description or title matching q
   try {
     const videos = await prisma.video.findMany({
@@ -67,6 +64,29 @@ search.get('/:q', async (req: Request, res: Response) => {
     res.status(200).send(results);
   } catch (err) {
     console.log('Error from search:\n', err);
+    res.sendStatus(500);
+  }
+});
+
+search.get('/party/:videoId', async (req: Request, res: Response) => {
+  const { videoId } = req.params;
+  try {
+    // find all the parties with a playlist that contains the video
+    const parties = await prisma.video.findUnique({
+      where: {
+        id: videoId,
+      },
+      include: {
+        playlists: {
+          include: {
+            parties: true,
+          },
+        },
+      },
+    });
+    res.status(200).send(parties);
+  } catch (err) {
+    console.log('The error from :videoId endpoint:\n', err);
     res.sendStatus(500);
   }
 });
