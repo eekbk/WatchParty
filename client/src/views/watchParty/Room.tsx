b51a416ff7c4f40fcb0409d1b92a5b3ab23070c4
eekbk <eekeric@gmail.com>
2022-10-08 22:50:33 -0500

    add: location to get some state from search

--- client/src/views/watchParty/Room.tsx
+++ client/src/views/watchParty/Room.tsx
@@ -1,11 +1,47 @@
+import { useEffect, useState, useContext } from 'react';
+import { useLocation } from 'react-router-dom';
 import {
   Card, Container, Row, Col,
 } from 'react-bootstrap';
+import axios from 'axios';
+import { UserContext } from '../../context';
 
 const { default: Video } = require('./Video.tsx');
 const { default: Chat } = require('./Chat.tsx');
 
 function WatchParty({ videos, user, room }) {
+  // function WatchParty() {
+  // const [roomId, setRoomId] = useState('');
+  const [playListVideos, setPlaylistVideos] = useState([]);
+  const [party, setParty] = useState(null);
+
+  const currentUser = useContext(UserContext);
+  const { state } = useLocation();
+
+  const eraseThisFuncOnceYouUsePartyAndPlaylistVideos = () => {
+    console.log(party, playListVideos, currentUser);
+  };
+
+  useEffect(() => {
+    // get props out of useLocation
+    setParty(state.party);
+    console.log('state.party', state.party);
+    // axios request to get vids based on roomId
+    axios
+      .get(`/api/watchParty/playlist/${state.party.id}`)
+      .then(({ data }) => {
+        console.log('data in useEffect:\n', data);
+        setPlaylistVideos([1]);
+      })
+      .catch((err) => {
+        console.error('This is the error in useEffect:\n', err);
+      });
+    // erase this next line when you use party and playlistVideos
+    if (party === playListVideos) {
+      eraseThisFuncOnceYouUsePartyAndPlaylistVideos();
+    }
+  }, []);
+
   return (
     <Container
       style={{
