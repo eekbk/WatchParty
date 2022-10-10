837323b83d25bfff4ebc65337b2e920e4ab52389
Logan-Ward <ward_logan@outlook.com>
2022-10-10 09:16:28 -0500

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -32,7 +32,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `http://localhost:${PORT}/auth/google/callback`,
+      callbackURL: `https://watch-party.azurewebsites.net/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       const user = await prisma.user.findUnique({
