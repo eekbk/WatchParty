607f59e2cfa53007cd77f8b140a53765d7ebeb28
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-10-11 11:17:13 -0500

    checking user profile glitch

--- server/index.ts
+++ server/index.ts
@@ -32,7 +32,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `https://watch-party.azurewebsites.net/auth/google/callback`,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       const user = await prisma.user.findUnique({
