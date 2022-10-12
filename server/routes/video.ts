0191c8544e84a28f615d2d35b52d60af983625dc
eekbk <eekeric@gmail.com>
2022-10-11 21:45:23 -0500

    add: route for video

---
+++ server/routes/video.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response, Router } from 'express';
+import { prisma } from '../db/index';
+
+export const video: Router = express.Router();
+
+video.get('/playlists/:videoId', (req: Request, res: Response) => {
+  const { videoId } = req.params;
+  prisma.playlist_Video
+    .findMany({
+      where: {
+        video_id: videoId,
+      },
+      include: {
+        playlist: {
+          include: {
+            parties: true,
+          },
+        },
+      },
+    })
+    .then((parties) => {
+      console.log('the parties:\n', parties);
+      res.status(200).send(parties);
+    })
+    .catch((err) => {
+      console.log('err', err);
+      res.sendStatus(500);
+    });
+});
