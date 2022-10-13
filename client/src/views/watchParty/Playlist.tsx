b9a76eea1d010759c53fb8c795733b0edeaf2871
Logan-Ward <ward_logan@outlook.com>
2022-10-13 11:36:26 -0500

    WIP: playlist for adding and removing videos from existing room

---
+++ client/src/views/watchParty/Playlist.tsx
@@ -0,0 +1,110 @@
+import { ListGroup, CloseButton, Form } from 'react-bootstrap';
+import { useState } from 'react';
+import axios from 'axios';
+import { StyledVideoCard, StyledButton } from '../../styles';
+import { StyledListGroup } from './styles';
+
+const { Group, Control, Text } = Form;
+const { Item } = ListGroup;
+
+export function Playlist({ playlist, setPlaylist, room }) {
+  const [video, setVideo] = useState('');
+  const [clicked, setClicked] = useState(false);
+  const handleVideoAddition = () => {
+    const regExp =			/^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    const match = video.match(regExp);
+    const videoId = match[2];
+    const videoUrl = video;
+    if (match && match[2].length === 11) {
+      axios
+        .post('/api/party/video', { videoId, videoUrl })
+        .then((vd) => {
+          setVideo('');
+          setPlaylist(playlist.concat([vd.data]));
+          // update playlist in database at post to api/playlist/video
+        })
+        .catch((err) => {
+          console.error(err);
+          setVideo('');
+        });
+    } else {
+      setVideo('');
+    }
+  };
+
+  const handleVideoRemoval = (i) => {
+    const vids = playlist.slice();
+    console.log(room);
+    axios
+      .put('/api/playlist/video', {
+        video_id: vids[i].id,
+        playlist_id: room.playlist_id,
+      })
+      .then(() => {
+        vids.splice(i, 1);
+        setPlaylist(vids);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <StyledListGroup
+      id="dropdown-basic-button"
+      title="Playlist"
+      style={{
+			  overflowY: 'scroll',
+			  maxHeight: '85%',
+			  width: '30%',
+			  position: 'absolute',
+      }}
+    >
+      <Item
+        style={{
+				  display: 'flex',
+				  justifyContent: 'space-between',
+				  height: '4vh',
+        }}
+      >
+        <h5 style={{ alignSelf: 'center' }}>Playlist</h5>
+        <StyledButton onClick={() => setClicked(!clicked)}>
+          {clicked ? 'Close' : 'Open'}
+        </StyledButton>
+      </Item>
+      {playlist.map((video, i) => (
+        <Item hidden={!clicked}>
+          <StyledVideoCard>
+            <CloseButton onClick={() => handleVideoRemoval(i)} />
+            <StyledVideoCard.Title>{video.title}</StyledVideoCard.Title>
+            <StyledVideoCard.Body>
+              <StyledVideoCard.Img src={video.thumbnail} />
+              <StyledVideoCard.Text>
+                {console.log(video)}
+                {video.description.slice(0, 150)}
+              </StyledVideoCard.Text>
+            </StyledVideoCard.Body>
+          </StyledVideoCard>
+        </Item>
+      ))}
+      <Item hidden={!clicked}>
+        <Form>
+          <Group>
+            <Control
+              placeholder="Paste Url Here"
+              onChange={(e) => setVideo(e.target.value)}
+              value={video}
+            />
+            <Group style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Text>Choose a youtube video to add</Text>
+              <StyledButton
+                style={{ marginLeft: '5px' }}
+                onClick={handleVideoAddition}
+              >
+                Add
+              </StyledButton>
+            </Group>
+          </Group>
+        </Form>
+      </Item>
+    </StyledListGroup>
+  );
+}
