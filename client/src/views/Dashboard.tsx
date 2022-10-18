b4f9b3aa790076fee8a4f2e92f4726ee7fdd70c0
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-10-18 14:15:02 -0500

    refactored/ changed layout to have following parties displayed

--- client/src/views/Dashboard.tsx
+++ client/src/views/Dashboard.tsx
@@ -3,23 +3,25 @@ import { Container } from 'react-bootstrap';
 import axios from 'axios';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import { useNavigate } from 'react-router-dom';
-import ListGroup from 'react-bootstrap/ListGroup';
+// import ListGroup from 'react-bootstrap/ListGroup';
 import { UserContext } from '../context';
 
 function Dashboard() {
   const { user } = useContext(UserContext);
   const [parties, setParties] = useState([]);
+  const [allParties, setAllParties] = useState([]);
   const navigate = useNavigate();
 
+  // all parties
   useEffect(() => {
     axios
       .get('/api/party')
       .then((data: any) => {
-        setParties(
+        setAllParties(
           data.data.sort(
             (a, b) =>
               Number(new Date(a.date_time)) - Number(new Date(b.date_time))
@@ -30,20 +32,37 @@ function Dashboard() {
         console.error(err);
       });
   }, []);
+  // parties of people im following
+  useEffect(() => {
+    if (user) {
+      axios
+        .get('/api/party')
+        .then((data: any) => {
+          setParties(
+            data.data
+              .filter((pt) =>
+                pt.users.some((ptU) =>
+                  user.following.some(
+                    (f) => f === ptU.id && ptU.role === 'CREATOR'
+                  )
+                )
+              )
+              .sort(
+                (a, b) =>
+                  Number(new Date(a.date_time)) - Number(new Date(b.date_time))
+              )
+          );
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }, [user]);
 
   const handleCardClick = (party) => {
     navigate('/watchParty', {
-      state: { party },
+      state: { party, videos: party.videos },
     });
-  };
-  const handleProfileClick = () => {
-    navigate('/profile');
-  };
-  const handleCalendarClick = () => {
-    navigate('/calendar');
-  };
-  const handleScrollClick = () => {
-    navigate('/profile');
   };
 
   return (
@@ -52,89 +71,55 @@ function Dashboard() {
       <Row />
       {user ? (
         <Row>
-          <Col sm={8}>
-            <Card style={{ width: '14rem' }}>
-              <Card.Img variant="top" src={user.profile} />
-              <Card.Body>
-                <Card.Title>
-                  {user.user_name}
-                  &apos;s dashboard
-                </Card.Title>
-                <Card.Text>Trick or Treat</Card.Text>
-                <Button variant="primary" onClick={() => handleProfileClick()}>
-                  profile
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col />
-          <Col sm={3}>
-            <ListGroup
-              style={{
-                overflowY: 'scroll',
-              }}
-            >
-              <ListGroup.Item action variant="dark">
-                <Col>Upcoming Watch Parties</Col>
-                <Col>
-                  <Row>
-                    <Col>
-                      <Button
-                        variant="primary"
-                        onClick={() => handleCalendarClick()}
-                      >
-                        Calendar
-                      </Button>
-                    </Col>
-                    <Col>
-                      <Button
-                        variant="primary"
-                        onClick={() => handleScrollClick()}
-                      >
-                        profile
-                      </Button>
-                    </Col>
-                  </Row>
-                </Col>
-              </ListGroup.Item>
-              {parties.slice(0, 5).map((party) => (
-                <ListGroup.Item
-                  action
-                  variant="light"
-                  onClick={() => handleCardClick(party)}
-                  key={party}
-                >
-                  <Col>{party.date_time.slice(5, 10)}</Col> 
-                  {' '}
-                  {party.name}
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-          </Col>
+          <Row>My upcoming follows party</Row>
+          {parties && parties.length ? (
+            <Col>
+              <CardGroup>
+                {parties.slice(0, 5).map((party) => (
+                  <Card key={party.id} onClick={() => handleCardClick(party)}>
+                    <Card.Img variant="top" src={party.thumbnail} />
+                    <Card.Body>
+                      <Card.Title>{party.name}</Card.Title>
+                      <Card.Text>{party.description}</Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                      <small className="text-muted">
+                        Starting on: 
+                        {' '}
+                        {party.date_time.slice(0, 10)}
+                      </small>
+                    </Card.Footer>
+                  </Card>
+                ))}
+              </CardGroup>
+            </Col>
+          ) : null}
         </Row>
       ) : null}
-      <Row> Top Rooms</Row>
+      <Row> Top parties</Row>
       {parties && parties.length ? (
-        <Col>
-          <CardGroup>
-            {parties.slice(0, 5).map((party) => (
-              <Card onClick={() => handleCardClick(party)}>
-                <Card.Img variant="top" src={party.thumbnail} />
-                <Card.Body>
-                  <Card.Title>{party.name}</Card.Title>
-                  <Card.Text>{party.description}</Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  <small className="text-muted">
-                    Starting on: 
-                    {' '}
-                    {party.date_time.slice(0, 10)}
-                  </small>
-                </Card.Footer>
-              </Card>
-            ))}
-          </CardGroup>
-        </Col>
+        <Row>
+          <Col>
+            <CardGroup>
+              {allParties.slice(0, 5).map((party) => (
+                <Card key={party.id} onClick={() => handleCardClick(party)}>
+                  <Card.Img variant="top" src={party.thumbnail} />
+                  <Card.Body>
+                    <Card.Title>{party.name}</Card.Title>
+                    <Card.Text>{party.description}</Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <small className="text-muted">
+                      Starting on: 
+                      {' '}
+                      {party.date_time.slice(0, 10)}
+                    </small>
+                  </Card.Footer>
+                </Card>
+              ))}
+            </CardGroup>
+          </Col>
+        </Row>
       ) : null}
     </Container>
   );
