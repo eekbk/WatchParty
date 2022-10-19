7cb2dff01bf844285eb1a18a96d764573a8af978
eekbk <eekeric@gmail.com>
2022-10-18 23:13:55 -0500

    update: search styling

--- client/src/views/search/search.styles.tsx
+++ client/src/views/search/search.styles.tsx
@@ -1,19 +1,26 @@
 import styled from 'styled-components';
-import { Button, Form, Container, InputGroup } from 'react-bootstrap';
+import {
+  Button,
+  Form,
+  Container,
+  InputGroup,
+  Card,
+  Image,
+} from 'react-bootstrap';
 
 export const SearchBarContainer = styled(Container)`
   display: flex;
   flex-direction: column;
-  width: 32rem;
+  width: 28rem;
   height: 3.8em;
   background-color: transparent;
-
+  padding-top: 12px;
   overflow: hidden;
 `;
 
 export const SearchInputContainer = styled(InputGroup)`
   width: 100%;
-  min-height: 4em;
+  /* min-height: 4em; */
   display: flex;
   align-items: center;
   position: relative;
@@ -25,13 +32,13 @@ export const SearchForm = styled(Form.Control)`
   height: 100%;
   outline: none;
   border: none;
-  font-size: 14px;
+  font-size: 15px;
   color: #12112e;
   font-weight: 500;
   border-radius: 20px;
   background-color: white;
-  padding: 9px;
-  /* border-radius: 6px; */
+  padding: 8px;
+
   box-shadow: 0px 2px 12px 3px rgba(0, 0, 0, 0.14);
   /* vertical-align: middle; */
 
@@ -50,12 +57,13 @@ export const SearchForm = styled(Form.Control)`
 
 export const SearchIcon = styled.span`
   color: #bebebe;
-  font-size: 16px;
+  font-size: 15px;
   background-color: white;
   /* margin-right: 10px;
   margin-top: 6px; */
   vertical-align: 10%;
   align-items: center;
+  padding-right: 4px;
 `;
 
 export const SearchButton = styled(Button)`
@@ -73,3 +81,29 @@ export const SearchButton = styled(Button)`
 
   /* vertical-align: middle; */
 `;
+
+export const VideoCard = styled(Card)`
+  height: 20rem;
+  width: 18rem;
+  padding: 0 auto;
+  cursor: pointer;
+`;
+
+export const StyledUserCard = styled(Card)`
+  width: 18rem;
+  height: 10rem;
+`;
+
+export const StyledUserCardImg = styled(Image)`
+  width: 100px;
+  height: 100px;
+  margin: 8px;
+`;
+
+export const StyledCardBody = styled(Card.Body)`
+  padding-left: 20px;
+  margin: 0 auto;
+  /* text-align: right; */
+`;
+
+export const StyledCardButton = styled(Button);
