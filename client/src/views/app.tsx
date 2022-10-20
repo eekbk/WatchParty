d1697e071e58890c458c95d71ebe2af6650490ce
Logan-Ward <ward_logan@outlook.com>
2022-10-20 18:34:25 -0500

    feat: app now properly checks if the user is logged in and redirects them to the homepage if not

--- client/src/views/app.tsx
+++ client/src/views/app.tsx
@@ -1,4 +1,4 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useNavigate } from 'react-router-dom';
 import { Nav, Navbar, Container, Button } from 'react-bootstrap';
 import { useContext, useEffect } from 'react';
 import axios from 'axios';
@@ -9,20 +9,25 @@ import VoiceControl from './voiceControl/VoiceControl';
 
 function App() {
   const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
   useEffect(() => {
-    axios
-      .get('/api/user')
-      .then((data) => {
-        setUser(data.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
+    if (!user) {
+      axios
+        .get('/api/user')
+        .then((data) => {
+          setUser(data.data);
+        })
+        .catch((err) => {
+          setUser(null);
+          navigate('/');
+          console.error(err);
+        });
+    }
+  }, [user]);
 
   const handleLogout = () => {
     axios
-      .get('/logout')
+      .post('/logout')
       .then(() => {
         setUser(null);
       })
@@ -35,28 +40,43 @@ function App() {
     <StyledBackgroundContainer fluid>
       <Navbar expand="lg" style={{ height: '10vh' }}>
         <Container>
-          <Navbar.Brand to="/" as={Link}>
+          <Navbar.Brand to="/" as={Link} style={{ color: '#E5F4E3' }}>
             WatchParty
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link hidden={!user} to="/createParty" as={Link}>
+              <Nav.Link
+                hidden={!user}
+                to="/createParty"
+                as={Link}
+                style={{ color: '#E5F4E3' }}
+              >
                 Create Party
               </Nav.Link>
-              <Nav.Link to="/profile" as={Link}>
+              <Nav.Link to="/profile" as={Link} style={{ color: '#E5F4E3' }}>
                 Profile
               </Nav.Link>
-              <Nav.Link to="/dm" as={Link}>
+              <Nav.Link to="/dm" as={Link} style={{ color: '#E5F4E3' }}>
                 DMs
               </Nav.Link>
-              <Nav.Link to="/archive" as={Link}>
+              <Nav.Link to="/archive" as={Link} style={{ color: '#E5F4E3' }}>
                 Archives
               </Nav.Link>
-              <Nav.Link hidden={user} as={Button} href="/auth/google">
+              <Nav.Link
+                hidden={user}
+                as={Button}
+                href="/auth/google"
+                style={{ color: '#E5F4E3' }}
+              >
                 Login
               </Nav.Link>
-              <Nav.Link hidden={!user} as={Button} onClick={handleLogout}>
+              <Nav.Link
+                hidden={!user}
+                as={Button}
+                onClick={handleLogout}
+                style={{ color: '#E5F4E3' }}
+              >
                 Logout
               </Nav.Link>
             </Nav>
