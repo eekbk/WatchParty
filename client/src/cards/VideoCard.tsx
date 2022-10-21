42e3a9c55699ac84a6cffc2aaaa444d483c7e501
eekbk <eekeric@gmail.com>
2022-10-20 23:50:42 -0500

    move: VideoCard

---
+++ client/src/cards/VideoCard.tsx
@@ -0,0 +1,65 @@
+import { useContext } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { UserContext } from '../context';
+import { SearchContext } from '../contexts/searchContext';
+import {
+  StyledVideoCard,
+  StyledCardBody,
+  StyledVideoCardImg,
+  StyledVideoPartyCount,
+  StyledVideoTitle,
+  StyledCardRow,
+  StyledCardCol,
+} from './cards.styles';
+
+// ModCard stands for Modular Card. Hopefully we can reuse it
+function VideoCard({ video }) {
+  const { user } = useContext(UserContext);
+  const { thumbnail, parties, title } = video;
+  // console.log('here are the props...\nobj:', obj, '\nkind:', kind);
+  const { setPartiesMatch, setUsersMatch, setVideosMatch } =
+    useContext(SearchContext);
+
+  const footerText =
+    parties.length === 1
+      ? `feat. in ${parties.length} party!`
+      : `feat. in ${parties.length} parties!`;
+
+  // const [cardTitle, setCardTitle] = useState('');
+  // const [cardText, setCardText] = useState('');
+  // const [follows, setFollows] = useState(0);
+  // const [isFollowing, setIsFollowing] = useState(false);
+  // const [isBlocking, setIsBlocking] = useState(false);
+
+  // set the parties count
+  // useEffect(() => {
+  //   axios.get(`/api/search/party/${video.id}`)
+  // }, []);
+
+  const handleCardClick = async () => {
+    // console.log(video.parties);
+    await setPartiesMatch(video.parties);
+    await setUsersMatch([]);
+    await setVideosMatch([]);
+  };
+
+  const cardTitle = title.length > 100 ? `${title.slice(0, 100)}...` : title;
+
+  // console.log('hello there');
+  return (
+    <StyledVideoCard onClick={handleCardClick}>
+      <StyledCardRow className="no-gutters">
+        <StyledCardCol sm={4}>
+          <StyledVideoCardImg src={thumbnail} />
+        </StyledCardCol>
+        <Col lg={8}>
+          <Row>
+            <StyledVideoTitle>{cardTitle}</StyledVideoTitle>
+          </Row>
+        </Col>
+      </StyledCardRow>
+    </StyledVideoCard>
+  );
+}
+
+export default VideoCard;
