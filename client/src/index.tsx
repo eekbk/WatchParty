b08053deea717e07ff619b5615fe88b662ce8cb9
eekbk <eekeric@gmail.com>
2022-11-02 13:41:07 -0500

    update: search route with params

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -12,10 +12,14 @@ import App from './views/app';
 import WatchParty from './views/watchParty/Room';
 import Dashboard from './views/Dashboard';
 // import Profile from './views/Profile';
-import Search from './views/search/Search';
+// import SearchTemp from './views/search/SearchTemp';
 import Dm from './views/Dm/Dm';
 import Archive from './views/Archive/Archive';
 import Calendar from './views/Calendar';
+import Search from './views/search/Search';
+// import PartiesResults from './views/moreResults/PartiesResults';
+// import UsersResults from './views/moreResults/UsersResults';
+// import VideosResults from './views/moreResults/VideosResults';
 
 const socket = io();
 
@@ -26,13 +30,20 @@ function RouteHandler() {
         <Routes>
           <Route path="/" element={<App />}>
             <Route path="" element={<Dashboard />} />
-            <Route path="search" element={<Search socket={socket} />} />
+            {/* <Route path="search" element={<Search socket={socket} />} /> */}
+            <Route path="search">
+              <Route path=":q" element={<Search socket={socket} />} />
+            </Route>
             <Route path="calendar" element={<Calendar />} />
             <Route path="watchParty" element={<WatchParty socket={socket} />} />
             <Route path="createParty" element={<CreateParty />} />
             {/* <Route path="profile" element={<Profile />} /> */}
             <Route path="dm" element={<Dm socket={socket} room="" />} />
             <Route path="archive" element={<Archive />} />
+            {/* <Route path="results/parties" element={<PartiesResults/>} />
+            <Route path="results/users" element={<UsersResults socket={socket} />} />
+            <Route path="results/videos" element={<VideosResults/>} /> */}
+            {/* <Route path="*" element={<App/>} /> */}
           </Route>
         </Routes>
       </BrowserRouter>
