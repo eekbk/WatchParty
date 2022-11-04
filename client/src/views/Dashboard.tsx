3505b0a628c3e3c1a1f53eddc72419e3b23f5985
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-11-04 14:58:48 -0500

    changed rows and cols/ now more responsive/ needs row banner tweak

--- client/src/views/Dashboard.tsx
+++ client/src/views/Dashboard.tsx
@@ -3,21 +3,13 @@ import { Container } from 'react-bootstrap';
 import axios from 'axios';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-// import Button from 'react-bootstrap/Button';
-// import Card from 'react-bootstrap/Card';
-// import CardGroup from 'react-bootstrap/CardGroup';
-// import DashboardPartyCard from '../cards/DashboardPartyCard';
 import PartyCard from '../components/cards/PartyCard';
-// import { useNavigate } from 'react-router-dom';
-// import ListGroup from 'react-bootstrap/ListGroup';
 import { UserContext } from '../context';
 
 function Dashboard() {
   const { user } = useContext(UserContext);
   const [parties, setParties] = useState([]);
   const [allParties, setAllParties] = useState([]);
-  // const [userParties, setUserParties] = useState([]);
-  // const navigate = useNavigate();
 
   // all parties
   useEffect(() => {
@@ -26,7 +18,6 @@ function Dashboard() {
       .then((data: any) => {
         const today = new Date();
         today.setHours(0, 0, 0, 0);
-        console.log(data.data, 'data.data...');
         setAllParties(
           data.data
             .sort(
@@ -73,7 +64,6 @@ function Dashboard() {
               )
           );
         })
-        .then(() => console.log(parties, 'parties.......'))
         .catch((err) => {
           console.error(err);
         });
@@ -82,108 +72,89 @@ function Dashboard() {
 
   return (
     <Container>
-      {user ? (
-        <Row>
-          {parties && parties.length ? (
-            <Col>
-              <Row
-                style={{ fontSize: '24px', padding: '10px', color: 'white' }}
-                className="text-center"
-              >
-                <hr
-                  style={{
-                    background: 'lime',
-                    color: 'lime',
-                    borderColor: 'lime',
-                    height: '5px',
-                  }}
-                />
-                <Col>My Upcoming Parties</Col>
-              </Row>
-              <Row
-                style={{
-                  justifyContent: 'center',
-                  justifyItems: 'center',
-                  paddingTop: '2rem',
-                }}
-              >
-                {user.parties
-                  .filter(
-                    // to get only the today and upcoming parties
-                    (a) => {
-                      const today = new Date();
-                      today.setHours(0, 0, 0, 0);
-                      return (
-                        Number(new Date(a.date_time)) >= Number(today) &&
-                        a.type === 'PARTY'
-                      );
-                    }
-                  )
-                  .sort(
-                    (a, b) =>
-                      Number(new Date(a.date_time)) -
-                      Number(new Date(b.date_time))
-                  )
-                  .slice(0, 4)
-                  .map((party) => (
-                    <Col
-                      style={{
-                        borderRadius: '1px',
-                        justifyContent: 'center',
-                        justifyItems: 'center',
-                        paddingTop: '2rem',
-                      }}
-                      key={party.id}
-                    >
-                      <PartyCard party={party} />
-                    </Col>
-                  ))}
-              </Row>
-            </Col>
-          ) : null}
-        </Row>
-      ) : null}
-      <Row
-        style={{ fontSize: '24px', padding: '10px', color: 'white' }}
-        className="text-center"
-      >
-        <hr
-          style={{
-            background: 'lime',
-            color: 'lime',
-            borderColor: 'lime',
-            height: '5px',
-          }}
-        />
-        <Col> Top parties</Col>
-      </Row>
-      <Row
-        style={{
-          justifyContent: 'center',
-          justifyItems: 'center',
-          paddingTop: '2rem',
-        }}
-      >
-        {allParties && allParties.length ? (
-          <Col>
-            <Row>
-              {allParties.slice(0, 4).map((party) => (
+      {parties && parties.length && allParties && allParties.length ? (
+        <Container>
+          <Container>
+            <p
+              style={{
+                height: '40px',
+                textAlign: 'center',
+                backgroundColor: '#320E3B',
+                fontSize: '30px',
+              }}
+            >
+              My Upcoming Parties
+            </p>
+          </Container>
+          <Row style={{ justifyContent: 'center' }}>
+            {user.parties
+              .filter(
+                // to get only the today and upcoming parties
+                (a) => {
+                  const today = new Date();
+                  today.setHours(0, 0, 0, 0);
+                  return (
+                    Number(new Date(a.date_time)) >= Number(today) &&
+                    a.type === 'PARTY'
+                  );
+                }
+              )
+              .sort(
+                (a, b) =>
+                  Number(new Date(a.date_time)) - Number(new Date(b.date_time))
+              )
+              .slice(0, 4)
+              .map((party) => (
                 <Col
-                  key={party.id}
+                  xs={12}
+                  sm={12}
+                  md={4}
+                  lg={3}
+                  xl={3}
                   style={{
-                    borderRadius: '1px',
-                    justifyContent: 'center',
-                    justifyItems: 'center',
-                    paddingTop: '2rem',
+                    padding: '10px',
+                    paddingTop: '24px',
+                    width: 'fit-content',
                   }}
+                  key={party.id}
                 >
                   <PartyCard party={party} />
                 </Col>
               ))}
-            </Row>
-          </Col>
-        ) : null}
-      </Row>
+          </Row>
+          <Container>
+            <p
+              style={{
+                height: '40px',
+                textAlign: 'center',
+                backgroundColor: '#320E3B',
+                fontSize: '30px',
+              }}
+            >
+              Top Parties
+            </p>
+          </Container>
+          <Row style={{ justifyContent: 'center' }}>
+            {allParties.slice(0, 4).map((party) => (
+              <Col
+                key={party.id}
+                xs={12}
+                sm={12}
+                md={4}
+                lg={3}
+                xl={3}
+                style={{
+                  padding: '10px',
+                  paddingTop: '24px',
+                  width: 'fit-content',
+                }}
+              >
+                <PartyCard party={party} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      ) : null}
     </Container>
   );
 }
