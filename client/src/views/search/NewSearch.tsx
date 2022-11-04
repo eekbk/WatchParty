ded78d0edff1163598f377ab4b54e037bf770290
eekbk <eekeric@gmail.com>
2022-11-04 09:53:40 -0500

    update: add pagination to party tab, remove unused code

--- client/src/views/search/NewSearch.tsx
+++ client/src/views/search/NewSearch.tsx
@@ -1,24 +1,25 @@
 import axios from 'axios';
 import { useContext, useEffect, useState } from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
-import { useNavigate, useParams } from 'react-router-dom';
+import { /* useNavigate, */ useParams } from 'react-router-dom';
 import UserCard from '../../components/cards/UserCard';
 import PartyCard from '../../components/cards/PartyCard';
 import VideoCard from '../../components/cards/VideoCard';
+import Paginator from '../../components/buttons/Paginator';
 // import { SearchContext } from '../../contexts/searchContext';
 import { UserContext } from '../../context';
-import { CategoryTitle } from '../../styles';
+// import { CategoryTitle } from '../../styles';
 import {
-  StyledCol,
+  // StyledCol,
   StyledRow,
   StyledTabs,
   StyledTab,
 } from '../CreateParty/styles';
 import {
-  SearchPageCol,
+  // SearchPageCol,
   SearchPageRow,
-  SearchPageHeading,
-  SeeMoreLink,
+  // SearchPageHeading,
+  // SeeMoreLink,
 } from './search.styles';
 // import VideoCard from '../../cards/VideoCard';
 
@@ -26,12 +27,18 @@ function Search({ socket }) {
   const [usersMatch, setUsersMatch] = useState([]);
   const [partiesMatch, setPartiesMatch] = useState([]);
   const [videosMatch, setVideosMatch] = useState([]);
+  const [userStartIndex, setUserStartIndex] = useState(0);
+  const [partyStartIndex, setPartyStartIndex] = useState(0);
+  const [videoStartIndex, setVideoStartIndex] = useState(0);
+
+  const [key, setKey] = useState('parties');
   const { user } = useContext(UserContext);
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const { q } = useParams();
 
   useEffect(() => {
     // do the axios search
+    console.log('partyStartIndex:', partyStartIndex);
     axios
       .get(`/api/search/${q}`)
       .then(({ data }) => {
@@ -39,87 +46,54 @@ function Search({ socket }) {
         setUsersMatch(data.users);
         setPartiesMatch(data.parties);
       })
-      // .then(() => {
-      //   navigate('/search');
-      // })
-      // .then(() => {
-      //   setIsLoading(true);
-      // })
       .catch((err) => {
         console.error('The Error from handleSubmit:', err);
       });
-  }, [q]);
+  }, [q, user, partyStartIndex]);
 
-  const seeMore = (category) => {
-    if (category === 'parties') {
-      navigate('/results/parties', {
-        state: { partiesMatch },
-      });
-    } else if (category === 'users') {
-      navigate('/results/users', {
-        state: { usersMatch },
-      });
-    } else if (category === 'videos') {
-      navigate('/results/videos', {
-        state: { videosMatch },
-      });
-    }
+  const paginate = (number, startIndexSetter) => {
+    console.log('er we in paginate');
+    console.log('partyStartIndex before', partyStartIndex);
+    console.log('number we changing it to:', number);
+    startIndexSetter(number);
+    console.log('partyStartIndex after', partyStartIndex);
   };
 
-  const handleSeeMore = (category) => {
-    seeMore(category);
-  };
-
-  // if there are parties and videos and users it should look one way
-  // otherwise if there are only videos and users is should look another way
-  // otherwise if there are only users, it should look another way
-
-  const usersResultsColumn = () => (
-    // we want to set a variable to the number of users allowed on the page
-    // if
-
-    <>
-      <SearchPageHeading>
-        <Col>
-          {usersMatch.length ? <CategoryTitle>Users</CategoryTitle> : []}
-        </Col>
-        <Col>
-          <SeeMoreLink onClick={() => handleSeeMore('users')}>
-            {usersMatch.length > 4 ? 'see more...' : []}
-          </SeeMoreLink>
-        </Col>
-      </SearchPageHeading>
-      <ul>
-        <Row>
-          {usersMatch
-            .filter(
-              (match) =>
-                !user.blockers.includes(match.id) &&
-                !user.blocking.includes(match.id)
-            )
-            .slice(0, 4)
-            .map((userMatch) => (
-              <Col md={6}>
-                <UserCard obj={userMatch} socket={socket} />
-              </Col>
-            ))}
-        </Row>
-      </ul>
-    </>
-  );
+  console.log('partiesMatch:', partiesMatch);
 
   return (
     <Container>
       <StyledRow>
-        <StyledTabs defaultActiveKey="party">
+        <StyledTabs
+          id="search-results-tabs"
+          activeKey={key}
+          onSelect={(k) => setKey(k)}
+        >
           <StyledTab eventKey="parties" title="Parties">
             <SearchPageRow>
-              {partiesMatch.slice(0, 4).map((party) => (
-                <Col xs={3}>
-                  <PartyCard party={party} />
-                </Col>
-              ))}
+              {partiesMatch
+                .filter((party) => {
+                  const creator = party.users.filter(
+                    (u) => u.role === 'CREATOR'
+                  )[0];
+                  return (
+                    !user.blockers.includes(creator.id) &&
+                    !user.blocking.includes(creator.id)
+                  );
+                })
+                .slice(partyStartIndex, partyStartIndex + 8)
+                .map((party) => (
+                  <Col xs={3}>
+                    <PartyCard party={party} />
+                  </Col>
+                ))}
             </SearchPageRow>
+            <Paginator
+              resultsPerPage={8}
+              totalResults={partiesMatch.length}
+              // paginate={paginate}
+              startIndexSetter={setPartyStartIndex}
+            />
           </StyledTab>
           <StyledTab eventKey="users" title="Users">
             {usersMatch
@@ -128,7 +102,7 @@ function Search({ socket }) {
                   !user.blockers.includes(match.id) &&
                   !user.blocking.includes(match.id)
               )
-              .slice(0, 4)
+              .slice(userStartIndex, userStartIndex + 12)
               .map((userMatch) => (
                 <Col md={6}>
                   <UserCard obj={userMatch} socket={socket} />
@@ -136,11 +110,13 @@ function Search({ socket }) {
               ))}
           </StyledTab>
           <StyledTab eventKey="videos" title="Videos">
-            {videosMatch.slice(0, 4).map((video) => (
-              <Row xs={3}>
-                <VideoCard video={video} key={video.id} />
-              </Row>
-            ))}
+            {videosMatch
+              .slice(videoStartIndex, videoStartIndex + 12)
+              .map((video) => (
+                <Row xs={3}>
+                  <VideoCard video={video} key={video.id} />
+                </Row>
+              ))}
           </StyledTab>
         </StyledTabs>
       </StyledRow>
