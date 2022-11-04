3aa89ebf589e58c6574e3e54602a63cab8bf9132
eekbk <eekeric@gmail.com>
2022-11-04 09:48:57 -0500

    add: Paginator component

---
+++ client/src/components/buttons/Paginator.tsx
@@ -0,0 +1,54 @@
+import { Pagination } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+
+function Paginator({ resultsPerPage, totalResults, startIndexSetter }) {
+  const [active, setActive] = useState(1);
+  const pages = [];
+  const pageCount = Math.ceil(totalResults / resultsPerPage);
+
+  for (let number = 1; number <= pageCount; number += 1) {
+    pages.push(
+      <Pagination.Item
+        key={number}
+        active={number === active}
+        onClick={() => {
+          console.log('booyah clicked');
+          console.log('active before:', active);
+          paginate(number);
+          console.log('active after:', active);
+        }}
+      >
+        {number}
+      </Pagination.Item>
+    );
+  }
+
+  const paginate = (number) => {
+    setActive(number);
+    startIndexSetter((number - 1) * resultsPerPage);
+  };
+
+  return (
+    <Pagination>
+      <Pagination.Prev
+        onClick={() => {
+          if (active > 1) {
+            paginate(active - 1);
+          }
+        }}
+        // hidden={active === 1 || pageCount <= 1}
+      />
+      {pages}
+      <Pagination.Next
+        onClick={() => {
+          if (active < pageCount) {
+            paginate(active + 1);
+          }
+        }}
+        //  hidden={active === pageCount || pageCount <= 1 || !pageCount}
+      />
+    </Pagination>
+  );
+}
+
+export default Paginator;
