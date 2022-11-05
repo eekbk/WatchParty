29486cbda5c84e5fa9c60b0d576a15594ac1b9b7
eekbk <eekeric@gmail.com>
2022-11-05 17:42:48 -0500

    add: voice control to help button

--- client/src/components/buttons/HelpButton.tsx
+++ client/src/components/buttons/HelpButton.tsx
@@ -1,13 +1,40 @@
-import { BsQuestionCircle, BsQuestion } from 'react-icons/bs';
-import { useState } from 'react';
+import { BsQuestion } from 'react-icons/bs';
+import { useState, useContext, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Modal } from 'react-bootstrap';
 import { StyledHelpButton } from './buttons.styles';
+import { VoiceContext } from '../../contexts/voiceContext';
 
 function HelpButton() {
   const [showHelpModal, setShowHelpModal] = useState(false);
+  const location = useLocation();
+  const { openModalToggle, closeModalToggle, listening } =
+    useContext(VoiceContext);
+
+  useEffect(() => {
+    if (listening) {
+      setShowHelpModal(true);
+    }
+  }, [openModalToggle]);
+
+  useEffect(() => {
+    setShowHelpModal(false);
+  }, [closeModalToggle]);
 
   const handleCloseHelpModal = () => setShowHelpModal(false);
   const handleOpenHelpModal = () => setShowHelpModal(true);
+
+  const generalHelpText = (
+    <p>
+      To search, say &quot;search for (party, username, or topic)&quot;, then
+      &quot;send&quot;.
+      <br />
+      To turn off microphone, say &quot;Stop listening&quot; or &quot;turn off
+      mic&quot;.
+      <br />
+      To clear the transcript, say &quot;clear&quot; or &quot;reset&quot;.
+    </p>
+  );
 
   return (
     <>
@@ -20,10 +47,8 @@ function HelpButton() {
         <Modal.Header closeButton>
           <Modal.Title>Voice Control Help</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          So in here I guess we can give some ideas. Maybe also we can
-          conditionally render based on where we are on site
-        </Modal.Body>
+        <Modal.Body>{generalHelpText}</Modal.Body>
+        <Modal.Footer>(Say &quot;exit&quot; to close this box)</Modal.Footer>
       </Modal>
       <StyledHelpButton onClick={handleOpenHelpModal}>
         <BsQuestion />
