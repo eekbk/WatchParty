d4412638deddc383e0befb736ec975890724d695
Logan-Ward <ward_logan@outlook.com>
2022-11-07 13:41:21 -0600

    refactor: calendar and dashboard are merged into a logged in and not logged in view

--- client/src/context.tsx
+++ client/src/context.tsx
@@ -4,12 +4,15 @@ import { createContext, useState, useMemo } from 'react';
 export const UserContext = createContext(null);
 export function UserContextProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [verified, setVerified] = useState(false);
   const userVal = useMemo(
     () => ({
       user,
       setUser,
+      verified,
+      setVerified,
     }),
-    [user, setUser]
+    [user, setUser, verified, setVerified]
   );
   return (
     <UserContext.Provider value={userVal}>{children}</UserContext.Provider>
