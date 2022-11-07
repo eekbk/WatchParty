aad2b06a7ba610746b9ef40a33ac3b71aeaa4522
Logan-Ward <ward_logan@outlook.com>
2022-11-07 10:45:23 -0600

    feat: debugged and formatted dashboard view

--- client/src/views/Dashboard.tsx
+++ client/src/views/Dashboard.tsx
@@ -18,143 +18,113 @@ function Dashboard() {
       .then((data: any) => {
         const today = new Date();
         today.setHours(0, 0, 0, 0);
-        setAllParties(
-          data.data
-            .sort(
-              (a, b) =>
-                Number(new Date(a.date_time)) - Number(new Date(b.date_time))
+        const tempParties = data.data
+          .filter(
+            // to get only the today and upcoming parties
+            // (a) => Number(new Date(a.date_time)) - Number(new Date()) > 0
+            (a) => Number(new Date(a.date_time)) >= Number(today)
+          )
+          .sort(
+            (a, b) =>
+              Number(new Date(a.date_time)) - Number(new Date(b.date_time))
+          );
+        setAllParties(tempParties);
+        if (user) {
+          setParties(
+            tempParties.filter((pt) =>
+              pt.users.some((ptU) =>
+                user.following.some(
+                  (f) => f === ptU.id && ptU.role === 'CREATOR'
+                )
+              )
             )
-            .filter(
-              // to get only the today and upcoming parties
-              // (a) => Number(new Date(a.date_time)) - Number(new Date()) > 0
-              (a) => Number(new Date(a.date_time)) >= Number(today)
-            )
-        );
+          );
+        }
       })
       .catch((err) => {
         console.error(err);
       });
   }, []);
-  // parties of people im following
+
   useEffect(() => {
     if (user) {
-      axios
-        .get('/api/party')
-        .then((data: any) => {
-          const today = new Date();
-          today.setHours(0, 0, 0, 0);
-          console.log(user, 'user...');
-          setParties(
-            data.data
-              .filter((pt) =>
-                pt.users.some((ptU) =>
-                  user.following.some(
-                    (f) => f === ptU.id && ptU.role === 'CREATOR'
-                  )
-                )
-              )
-              .sort(
-                (a, b) =>
-                  Number(new Date(a.date_time)) - Number(new Date(b.date_time))
-              )
-              .filter(
-                // to get only the today and upcoming parties
-                // (a) => Number(new Date(a.date_time)) - Number(new Date()) > 0
-                (a) => Number(new Date(a.date_time)) >= Number(today)
-              )
-          );
-        })
-        .catch((err) => {
-          console.error(err);
-        });
+      setParties(
+        allParties.filter((pt) =>
+          pt.users.some((ptU) =>
+            user.following.some((f) => f === ptU.id && ptU.role === 'CREATOR')
+          )
+        )
+      );
+    } else {
+      setParties([]);
     }
   }, [user]);
 
   return (
     <Container>
-      {parties && parties.length && allParties && allParties.length ? (
-        <Container>
-          <Container>
-            <p
-              style={{
-                height: '40px',
-                textAlign: 'center',
-                backgroundColor: '#320E3B',
-                fontSize: '30px',
-              }}
-            >
-              My Upcoming Parties
-            </p>
-          </Container>
-          <Row style={{ justifyContent: 'center' }}>
-            {user.parties
-              .filter(
-                // to get only the today and upcoming parties
-                (a) => {
-                  const today = new Date();
-                  today.setHours(0, 0, 0, 0);
-                  return (
-                    Number(new Date(a.date_time)) >= Number(today) &&
-                    a.type === 'PARTY'
-                  );
-                }
-              )
-              .sort(
-                (a, b) =>
-                  Number(new Date(a.date_time)) - Number(new Date(b.date_time))
-              )
-              .slice(0, 4)
-              .map((party) => (
-                <Col
-                  xs={12}
-                  sm={12}
-                  md={4}
-                  lg={3}
-                  xl={3}
-                  style={{
-                    padding: '10px',
-                    paddingTop: '24px',
-                    width: 'fit-content',
-                  }}
-                  key={party.id}
-                >
-                  <PartyCard party={party} />
-                </Col>
-              ))}
-          </Row>
-          <Container>
-            <p
-              style={{
-                height: '40px',
-                textAlign: 'center',
-                backgroundColor: '#320E3B',
-                fontSize: '30px',
-              }}
-            >
-              Top Parties
-            </p>
-          </Container>
-          <Row style={{ justifyContent: 'center' }}>
-            {allParties.slice(0, 4).map((party) => (
-              <Col
-                key={party.id}
-                xs={12}
-                sm={12}
-                md={4}
-                lg={3}
-                xl={3}
-                style={{
-                  padding: '10px',
-                  paddingTop: '24px',
-                  width: 'fit-content',
-                }}
-              >
-                <PartyCard party={party} />
-              </Col>
-            ))}
-          </Row>
-        </Container>
-      ) : null}
+      <Row style={{ justifyContent: 'center' }}>
+        <Col xs={12} hidden={!parties.length}>
+          <h2
+            style={{
+              height: '40px',
+              textAlign: 'center',
+              backgroundColor: '#320E3B',
+              fontSize: '30px',
+            }}
+          >
+            My Upcoming Parties
+          </h2>
+        </Col>
+        {parties.slice(0, 4).map((party) => (
+          <Col
+            xs={12}
+            sm={12}
+            md={4}
+            lg={3}
+            xl={3}
+            style={{
+              padding: '10px',
+              paddingTop: '24px',
+              width: 'fit-content',
+            }}
+            key={party.id}
+          >
+            <PartyCard party={party} />
+          </Col>
+        ))}
+      </Row>
+      <Row style={{ justifyContent: 'center' }}>
+        <Col xs={12}>
+          <h2
+            style={{
+              height: '40px',
+              textAlign: 'center',
+              backgroundColor: '#320E3B',
+              fontSize: '30px',
+            }}
+            hidden={!allParties.length}
+          >
+            Top Parties
+          </h2>
+        </Col>
+        {allParties.slice(0, 4).map((party) => (
+          <Col
+            key={party.id}
+            xs={12}
+            sm={12}
+            md={4}
+            lg={3}
+            xl={3}
+            style={{
+              padding: '10px',
+              paddingTop: '24px',
+              width: 'fit-content',
+            }}
+          >
+            <PartyCard party={party} />
+          </Col>
+        ))}
+      </Row>
     </Container>
   );
 }
